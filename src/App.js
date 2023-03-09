f583906d4ec0d8771cac86d788c82115496cd222
Siddharth Sappal <siddharthsappal@gmail.com>
2023-03-09 15:41:52 +0000

    greet 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ export default function App() {
   return (
     <div className="App">
       <Header/>
-      <h1>Hello Siddharth Sappal</h1>
+      <h1>Hello Sid</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
