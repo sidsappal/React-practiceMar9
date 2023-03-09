e081014ba8a4f51480bbad03ad965ebd10407802
Siddharth Sappal <siddharthsappal@gmail.com>
2023-03-09 15:26:11 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import "./styles.css";
+import React from 'react';
+import Header from './Components/Header.js';
 
 export default function App() {
   return (
     <div className="App">
+      <Header/>
       <h1>Hello Siddharth</h1>
       <h2>Start editing to see some magic happen!</h2>
     </div>
